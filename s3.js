529d1ced3cbcef952bc1ef8bb736ddad5e3c5926
Drew Synan <dsynan@gmail.com>
2017-01-17 19:08:14 -0600

    re-write some stuff, add tests

--- s3.js
+++ s3.js
@@ -1,3 +1,4 @@
+
 ;(function(definition){
 	if(typeof global === "object" && typeof module === "object") { // NodeJS
 		definition(module.exports);
@@ -7,127 +8,345 @@
 		throw new Error("Unsupported environment. Could not load s3-oo");
 	}
 }(function(ENV){
-var CLASS_ATTR 		= "__s3class__";
-var TRACKER_CLASS 	= "__s3classtracker__";
 
-function bless(x) {
-	x.useMethod 	= useMethod;
-	x.nextMethod 	= nextMethod;
-	return x;
+var DEBUG = true;
+var DEBUG_LEVEL = 99;
+var DEBUG_STACK = true;
+
+function dbg(/*args*/) {
+	/* Debug to the console if debugging is enabled */
+	/* Passes all called arguments on to console.debug */
+
+	if(DEBUG) {
+		var args = Array.prototype.slice.call(arguments);
+		console.debug.apply(console, [".:.: S3 :.:.\n"].concat(args));
+	}
 }
 
+function dbgFn(fn, thisScope, args, stack, debugLevel) {
+	/* Debug a function call if debugging is enabled */
+	/* fn => reference to named function or function object */
+	/* thisScope => the this property passed from inside the function */
+	/* args => arguments object passed from inside the function */
+	/* stack => stack trace passed from calling (new Error()).stack at the debug site */
+	/* debugLevel => which level of debugging to debug in, defaults to 0 (most verbose) */
+
+	args = Array.prototype.slice.call(args);
+	stack = stack || "";
+	debugLevel = +debugLevel;
+
+	function argNames(f) {
+		return (f + '')
+			.replace(/[/][/].*$/mg,'') // strip single-line comments
+      		.replace(/\s+/g, '') // strip white space
+      		.replace(/[/][*][^/*]*[*][/]/g, '') // strip multi-line comments  
+      		.split('){', 1)[0].replace(/^[^(]*[(]/, '') // extract the parameters  
+      		.replace(/=[^,]+/g, '') // strip any ES6 defaults  
+      		.split(',').filter(Boolean); // split & filter [""]
+	}
+
+	if(DEBUG_STACK) {
+		stack = "\n" + stack.replace(/Error\n/,"");
+	} else {
+		stack = "";
+	}
+
+	if(DEBUG && debugLevel >= DEBUG_LEVEL) {
+		var namedArgs = argNames(fn);
+		var prettyPrintArgs = [];
+		namedArgs.forEach(function(arg, i){
+			prettyPrintArgs.push("\n  "+arg+"->", args[i]);
+		});
+
+		console.debug.apply(console, 
+			[
+			 ".:.: S3 :.:.", 
+			 "\n" + fn.name + "(" + namedArgs.join(",") + ")", 
+			 "=>",
+			 "\n  (this)->",
+			 thisScope,
+			].concat(prettyPrintArgs, stack)
+		);
+	}
+
+}
+
+function environment() { return ENV.__env__; }
+
 function classOf(x, setter) {
-	function coerce(primitive) {
-		var obj = Object.valueOf.call(primitive);
-		obj[CLASS_ATTR] = typeof primitive;
+	dbgFn(classOf, this, arguments, (new Error()).stack, 1);
+
+	var CLASS_ATTR = "__s3class__";
+
+	function _set(obj, className) {
+		dbgFn(_set, this, arguments, (new Error()).stack);
+
+		obj[CLASS_ATTR] = [].concat(className);
 		return obj;
 	}
 
+	function _get(obj) {
+		dbgFn(_get, this, arguments, (new Error()).stack);
+
+		return obj[CLASS_ATTR];
+	}
+
+	function coerce(primitive) {
+		/* Force object representatino of primitives */
+		dbgFn(coerce, this, arguments, (new Error()).stack);
+
+		return _set(Object.valueOf.call(primitive), typeof primitive);
+	}
+
+	function getConstructor(obj) {
+		// could make this recurse up the __proto__ chain, but idk how useful that would actually be
+
+		if(obj.__proto__ && obj.__proto__.constructor && obj.__proto__.constructor.name) {
+			var constructorName = obj.__proto__.constructor.name;
+			var className = constructorName[0].toLowerCase() + constructorName.slice(1);
+			return className;
+		} else {
+			return typeof obj;
+		}
+	}
+
 	function getClass(obj) {
+		dbgFn(getClass, this, arguments, (new Error()).stack);
+
 		if (typeof obj === "undefined" || obj === null) { // undefined and null have no class
 			return undefined;
-		} else if (obj[CLASS_ATTR]) { 
-			return obj[CLASS_ATTR]; 
+		} else if (_get(obj)) { 
+			return _get(obj); 
 		} else if (Array.isArray(obj)) { 
 			return ["array"]; 
 		} else if (typeof obj === "object") { 
-			return ["object"]; 
+			return [getConstructor(obj)]; 
 		} else {
 			return [typeof obj];
-		}		
+		}
 	}
 
 	function setClass(obj, classes) {
+		dbgFn(setClass, this, arguments, (new Error()).stack);
+
 		// modifies in place, returns object copies
 		// of immutable values
+
+		// TODO: need a way of filtering/dealing with duplicate class names
 
 		if(!getClass(obj)) {
 			console.warn('Cannot set class of null or undefined');
 			return obj;
 		}
-		if(typeof obj !== "function" || typeof obj !== "object") {
+
+		// filter out extraneous function and object class 
+		// declarations
+
+		if(typeof obj !== "function" && typeof obj !== "object") {
 			obj = coerce(obj);
 		}
 
-		obj[CLASS_ATTR] = [].concat(classes);
-		return obj;
+		return _set(obj, classes);
 	}
 
 	return (setter === undefined ? getClass(x) : setClass(x, setter));
+
 }
 
-function environment() { return ENV; }
+function hasClass(x, c) {
+	dbgFn(hasClass, this, arguments, (new Error()).stack, 1);
 
-function failure(fail) { throw new Error("fail"); }
+	return classOf(x).indexOf(c) >= 0;
+}
 
-function generic(f) {
-	if( !f.name ) return failure("Name of function for generic method not given: s3.generic needs to be used with named functions.");
-	var g = bless(f).bind(bless({}));
-	ENV[f.name] = g;
+function lastClass(x) {
+	dbgFn(lastClass, this, arguments, (new Error()).stack, 1);
+
+	//return classOf(x)? classOf(x).slice(-1)[0]: undefined; /* class list reads from the end */
+	return classOf(x)? classOf(x)[0] : undefined; /* class list reads from the front (like R) */
+}
+
+function classBefore(o, c) {
+	dbgFn(classBefore, this, arguments, (new Error()).stack, 1);
+
+	var classList = classOf(o);
+	//return classList[classList.indexOf(c) - 1]; /* class list reads from the end */
+	return classList[classList.indexOf(c) + 1]; /* class list reads from the front (like R) */
+}
+
+function appendClass(obj, c) {
+	dbgFn(appendClass, this, arguments, (new Error()).stack, 1);
+
+	//return classOf(obj, classOf(obj).concat(c)); /* class list reads from the end */
+	return classOf(obj, [].concat(c, classOf(obj))); /* class list reads from the front (like R) */
+}
+
+function generic(f, name, env) {
+	dbgFn(generic, this, arguments, (new Error()).stack, 2);
+
+	if(!f.name && !name) {
+		throw new Error("Named function or name parameter not given. Call again with a named function, or pass a method name as the second argument");
+	}
+	var env = environment();
+	var methodName = name || f.name; // use overriding name before function name
+	var proto = Object.create(null);
+
+	proto.useMethod = useMethod;//maybeName.call(proto, useMethod);
+	proto.nextMethod = nextMethod;//maybeName.call(proto, nextMethod);
+	proto.methodName = methodName;
+	proto.dbgFn = dbgFn;
+
+	var expandedProto = Object.assign(f.__proto__, proto);
+	f.__proto__ = expandedProto;
+	var g = f.bind(proto);
+	appendClass(g, "genericFn");
+	env[methodName] = g;
+
+	//Object.assign(g, proto); 
+	/* make this.nextMethod and this.useMethod functions available when method.class functions are
+	   called directly without binding to a context */
 
 	return g;
 }
 
-function hasClass(x, c) { return classOf(x).indexOf(c) >= 0; }
+function method(methodName, className, obj/*, args...*/) {
+	dbgFn(method, this, arguments, (new Error()).stack, 2);
 
-function method(methodName, obj/*, args, ...*/) {
-	if(!hasClass(this, TRACKER_CLASS)) { return failure("className in classTracker not set !"); }
-	
-	var className = this.class;
 	var env = environment();
-	var objectClassList = classOf(obj);
-	var methodArgs = Array.prototype.slice.call(arguments).slice(1);
-
-	var method = env[methodName];
-	if(!method) { return failure("Method " + methodName + " is not defined."); }
-
-	var reifiedMethods = new Set(Object.keys(method));
-	var dispatched = reifiedMethods.has(className)? method[className] : method.default;
-
-	if(!dispatched) { return failure("Could not find " + methodName + " method for " + className); } 
-
-	var classTracker = bless({class: className});
-	classOf(classTracker, TRACKER_CLASS);
-
-	return dispatched.apply(classTracker, methodArgs);
-} 
-
-function nextMethod(methodName, obj/*, args ... */) {
 	var methodArgs = Array.prototype.slice.call(arguments).slice(2);
-	var classList = classOf(obj);
-	var baseClassName;
-
-	if(!hasClass(this, TRACKER_CLASS)) {
-		baseClassName = classList[classList.length - 1];
-	} else {
-		baseClassName = this.class;
+	if(!env[methodName]) {
+		throw new Error("Method " + methodName + " is not defined.");
 	}
 
-	var nextClassName = classList[classList.indexOf(baseClassName) - 1];
-	if(!nextClassName) { return failure("No next class for " + baseClassName)}
+	var dispatched = env[methodName][className] ? env[methodName][className] : env[methodName]['default'];
 
-	var classTracker = bless({class: nextClassName});
-	classOf(classTracker, TRACKER_CLASS);
+	if(!dispatched) {
+		throw new Error(methodName + "." + className + " method not defined.");
+	}
 
-	return method.apply(classTracker, [methodName, obj].concat(methodArgs));
+	var methodContext = Object.create(null);
+	classOf(methodContext, classOf(obj));
+	methodContext.className = className;
+	Object.assign(methodContext, this);
+
+	return dispatched.apply(methodContext, methodArgs);
 }
 
-function useMethod(methodName, obj/*, args... */) {
+function processArgs(thisScope, args) {
+	args = Array.prototype.slice.call(args);
+	var methodName, methodArgs, obj;
+
+	if(typeof args[0] === 'string' && classOf(args[1])) {
+		methodName = args[0];
+		methodArgs = args.slice(2);
+		obj = args[1];
+	} else {
+		methodName = thisScope.methodName;
+		methodArgs = args.slice(1);
+		obj = args[0];
+	}
+
+	if(!methodName) {
+		throw new Error("No method name passed (implicitly or explicitly).");
+	}
+
+	if(!classOf(obj)) {
+			throw new Error("No object passed to " + methodName + ".");
+	}
+
+	return {
+		methodName: methodName,
+		methodArgs: methodArgs,
+		obj: obj
+	};
+}
+
+function useMethod(maybeName, maybeObj/*, methodArgs... */) {
+	var args = processArgs(this, arguments);
+	var methodName = args.methodName;
+	var methodArgs = args.methodArgs;
+	var obj = args.obj;
+
+	dbgFn(useMethod, this, arguments, (new Error()).stack, 2);
+
+	if(typeof methodName !== 'string') {
+		throw new Error("useMethod: method name is not a string. Got " + methodName);
+	}
+	if(!classOf(obj)) {
+		throw new Error("useMethod: object not given for method use. Got " + obj);
+	}
+
 	var methodArgs = Array.prototype.slice.call(arguments).slice(2);
-	var className = classOf(obj).slice(-1)[0]; // last class, no ancestors
+	var className = lastClass(obj); // last class name in the class chain (i.e. most recent class added)
 
-	var classTracker = bless({class: className});
-	classOf(classTracker, TRACKER_CLASS);
+	return method.apply(this, [methodName, className, obj].concat(methodArgs));
+}
 
-	return method.apply(classTracker, [methodName, obj].concat(methodArgs));
+function nextMethod(maybeName, maybeObj/*, methodArgs... */) {
+	var args = processArgs(this, arguments);
+	var methodName = args.methodName;
+	var methodArgs = args.methodArgs;
+	var obj = args.obj;
+
+	dbgFn(nextMethod, this, arguments, (new Error()).stack, 2);
+
+	if(typeof methodName !== 'string') {
+		throw new Error("nextMethod: method name is not a string. Got " + methodName);
+	}
+	if(!classOf(obj)) {
+		throw new Error("nextMethod: object not given for method use. Got '" + obj + "'");
+	}
+
+	var methodArgs = Array.prototype.slice.call(arguments).slice(2);
+	var methodContext = Object.assign(Object.create(null), this);
+
+	if(!this.nextClass) {
+		methodContext.currentClass = lastClass(obj);
+		methodContext.nextClass = classBefore(obj, lastClass(obj));
+	} else {
+		methodContext.currentClass = this.nextClass;
+		methodContext.nextClass = classBefore(obj, this.nextClass);
+	}
+
+	if(!methodContext.nextClass) {
+		throw new Error("No next '" + methodName + "' method to call after nextMethod call from " + methodName + "." + methodContext.currentClass);
+	}
+
+	return method.apply(methodContext, [methodName, methodContext.nextClass, obj].concat(methodArgs));
+
+}
+
+function methods(nameOrFunc) {
+	if(typeof nameOrFunc === 'string') {
+		var foundMethod = this.__env__[nameOrFunc];
+		if(foundMethod) {
+			return Object.keys(foundMethod)
+				.filter(function(k){ return k !== "__s3class__"; })
+				.map(function(k){ return nameOrFunc + "." + k; });
+		} else {
+			return [];
+		}
+	} else {
+		return Object.keys(nameOrFunc)
+				.filter(function(k){ return k !== "__s3class__"; })
+				.map(function(k){ return nameOrFunc.name.replace(/bound /,"") + "." + k; });
+	}
 }
 
 var exports = {
-	generic: bless(generic),
-	classOf: classOf,
-	appendClass: function addClass(x, c) { return classOf(x, classOf(x).concat(c)); }
+	__env__: Object.create(null),
+	generic: appendClass(generic, "s3Builtin"),
+	classOf: appendClass(classOf, "s3Builtin"),
+	hasClass: appendClass(hasClass, "s3Builtin"),
+	appendClass: appendClass(appendClass, "s3Builtin"),
+	methods: appendClass(methods, "s3Builtin"),
+	lastClass: appendClass(lastClass, "s3Builtin"),
+	dbgFn: appendClass(dbgFn, "s3Builtin"),
+	dbg: appendClass(dbg, "s3Builtin")
 };
 
 Object.assign(ENV, exports);
 
 }));
+
+
---
+++ spec/s3_spec.js
@@ -0,0 +1,304 @@
+describe("s3 main object", function(){
+	var s3 = require('../s3-redo.js');
+	if("should be an object", function(){
+		expect(typeof s3).toEqual("object");
+	});
+
+	it("should have the following methods defined", function(){
+		expect(Object.keys(s3).sort()).toEqual([
+			"__env__", 
+			"appendClass", 
+			"classOf", 
+			"dbg", 
+			"dbgFn", 
+			"generic", 
+			"hasClass",
+			"lastClass", 
+			"methods"
+		]);
+	});
+});
+
+describe("classes of builtins and objects", function(){
+	var s3 = require('../s3-redo');
+
+	it("should class builtins (except for special cases of object) the same as using typeof", function(){
+		expect(s3.classOf('string')).toEqual(['string']);
+		expect(s3.classOf(3)).toEqual(['number']);
+		expect(s3.classOf(false)).toEqual(['boolean']);
+		expect(s3.classOf(function(){})).toEqual(['function']);
+		expect(s3.classOf({})).toEqual(['object']);
+	});
+
+	it("should deal with object versions of primitives", function(){
+		expect(s3.classOf(new String('string'))).toEqual(['string']);
+		expect(s3.classOf(new Number(3))).toEqual(['number']);
+		expect(s3.classOf(new Boolean(true))).toEqual(['boolean']);
+		expect(s3.classOf(new Function())).toEqual(['function']);
+	});
+
+	it("should not assign classes to null or undefined", function(){
+		expect(s3.classOf(undefined)).toBe(undefined);
+		expect(s3.classOf(null)).toBe(undefined);
+	});
+
+	it("should convert constructors of builtin classes", function(){
+			expect(s3.classOf(new Date())).toEqual(['date']);
+	});
+
+	it("should convert constructor names of user-defined classes", function(){
+			function MyOb() {
+				this.test = '1';
+			}
+			expect(s3.classOf(new MyOb())).toEqual(['myOb']);
+	});
+
+	it("should fallback to typeof when constructor names aren't available", function(){
+		expect(s3.classOf(new (function(){})())).toEqual(['object']);
+	});
+
+});
+
+describe("setting and modifying classes", function(){
+	var s3 = require('../s3-redo.js');
+	it("should be able set a class", function(){
+		var x = s3.classOf({}, "myClass");
+		expect(s3.classOf(x)).toEqual(["myClass"]);
+
+		var y = s3.classOf({}, ["myClass"]);
+		expect(s3.classOf(x)).toEqual(["myClass"]);
+	});
+	it("should return a new object when classing a primitive", function(){
+		var x = 3;
+		var y = s3.classOf(x, "myNumber");
+
+		expect(s3.classOf(x)).toEqual(['number']);
+		expect(x.__s3class__).toBe(undefined);
+
+		expect(y.__proto__.constructor.name).toBe("Number");
+		expect(s3.classOf(y)).toEqual(['myNumber']);
+	});
+	it("should be able to append a class to the class chain", function(){
+		var x = Object.create(null);
+		s3.appendClass(x, "myClass");
+		expect(s3.classOf(x)).toEqual(['myClass', 'object']);
+		expect(s3.lastClass(x)).toBe('myClass');
+
+		s3.appendClass(x, ["myThirdClass", "mySecondClass"]);
+		expect(s3.classOf(x)).toEqual(['myThirdClass', 'mySecondClass', 'myClass', 'object']);
+		expect(s3.lastClass(x)).toBe('myThirdClass');
+
+	});
+	it("should be be able to overwrite the entire class chain", function(){
+		var x = s3.classOf({}, ["firstClass", "secondClass"]);
+		expect(s3.classOf(x)).toEqual(["firstClass", "secondClass"]);
+
+		s3.classOf(x, ["thirdClass", "fourthClass"]);
+		expect(s3.classOf(x)).toEqual(["thirdClass", "fourthClass"]);
+	});
+	it("should be able to tell if an object has a class in the chain", function(){
+		var x = s3.classOf({}, ["firstClass", "secondClass"]);
+		expect(s3.hasClass(x, "firstClass")).toBe(true);
+		expect(s3.hasClass(x, "secondClass")).toBe(true);
+		expect(s3.hasClass(x, "object")).toBe(false);
+	});
+});
+
+describe("creating generic functions", function(){
+	var s3 = require('../s3-redo.js');
+
+	it("should be able to create a generic function", function(){
+		var info = s3.generic(function info(x){
+			this.useMethod(x);
+		});
+		expect(typeof info).toBe('function');
+		expect(s3.hasClass(info, 'genericFn')).toBe(true);
+		expect(typeof s3.__env__.info).toBe('function');
+	});
+
+	it("should have useMethod, nextMethod, and methodName available inside the generic function", function(){
+		var info = s3.generic(function info(x){
+			return this;
+		});
+
+		var infoThis = info();
+		expect(typeof infoThis.useMethod).toBe('function');
+		expect(typeof infoThis.nextMethod).toBe('function');
+		expect(typeof infoThis.methodName).toBe('string');
+
+	});
+
+	it("should be able to create a generic function from an external name parameter", function(){
+		var deets = s3.generic(function stuff(x){
+			return this.methodName;
+		}, 'nameFromParameter');
+
+		expect(deets()).toBe('nameFromParameter');
+		expect(deets.methodName).toBe('nameFromParameter');
+		expect(typeof s3.__env__.nameFromParameter).toBe('function');
+	});
+
+});
+
+describe("dispatching generic functions", function(){
+	var s3 = require('../s3-redo.js');
+
+	it("should be able to register class methods", function(){
+		var info = s3.generic(function info(x){
+			return this.useMethod(x);
+		});
+		info.string = function stringInfo() {
+			return "this is a string";
+		};
+		info.myClass = function myClassInfo() {
+			return "this is a myClass";
+		};
+
+		expect(s3.methods(info).sort()).toEqual(['info.myClass', 'info.string']);
+		expect(s3.methods('info').sort()).toEqual(['info.myClass', 'info.string']);
+
+	});
+
+	it("should be able to dispatch on type", function(){
+		var info = s3.generic(function info(x){
+			return this.useMethod(x);
+		});
+		info.string = function stringInfo(x) {
+			return "this is a string";
+		};
+		info.myClass = function myClassInfo(x) {
+			return "this is a myClass";
+		}
+
+		var s = s3.classOf({}, "string");
+		var m = s3.classOf({}, "myClass");
+
+		expect(info(s)).toBe("this is a string");
+		expect(info(m)).toBe("this is a myClass");
+
+	});
+
+	it("should be able to give an explicit method name when dispatching", function(){
+		var info = s3.generic(function info(x){
+			return this.useMethod('info', x);
+		});
+		info.string = function stringInfo(x) {
+			return "this is a string";
+		};
+		info.myClass = function myClassInfo(x) {
+			return this.useMethod('sekrit', x);
+		};
+
+		var sekrit = s3.generic(function sekrit(x){
+			return this.useMethod('sekrit', x);
+		});
+		sekrit.myClass = function sekritMyClass(x) {
+			return 'sekrit';
+		};
+
+		expect(info('hello')).toBe('this is a string');
+		expect(info(s3.classOf({},'myClass'))).toBe('sekrit');
+	});
+
+	it("should fail when a class method can't be dispatched", function(){
+		var info = s3.generic(function info(x){
+			return this.useMethod(x);
+		});
+		info.string = function stringInfo() {
+			return "this is a string";
+		};
+
+		expect(function() {
+			info(3)
+		}).toThrowError();
+	});
+
+	it("should be able to call a class method directly", function(){
+		var info = s3.generic(function info(x){
+			return this.useMethod(x);
+		});
+		info.myClass = function myInfo(x) {
+			return this.nextMethod(x);
+		};
+		info.string = function stringInfo() {
+			return "this is a string";
+		};
+
+		var myVal = s3.appendClass("hey there", "myClass");
+		expect(info.myClass(myVal)).toBe("this is a string");
+	});
+});
+
+describe("dispatching down the class chain", function(){
+	var s3 = require('../s3-redo.js');
+
+	it("should be able to dispatch on the next method in the prototype chain", function(){
+		var info = s3.generic(function info(x){
+			return this.useMethod(x);
+		});
+		info.myClass = function myInfo(x) {
+			return "myClassWasHere: " + this.nextMethod(x);
+		}
+		info.string = function stringInfo() {
+			return "this is a string";
+		}
+
+		var myVal = s3.appendClass("Suzanne", "myClass");
+
+		expect(info(myVal)).toBe("myClassWasHere: this is a string");
+
+	});
+
+	it("should throw an error when there isn't a next method defined", function(){
+		var info = s3.generic(function info(x){
+			return this.useMethod(x);
+		});
+		info.myClass = function myInfo(x) {
+			return this.nextMethod(x);
+		};
+
+		var myVal = s3.appendClass("Suzanne", "myClass");
+
+		expect(function(){ info(myVal)}).toThrowError();
+	});
+	it("should throw an error when there isn't a next class in the chain", function(){
+		var info = s3.generic(function info(x){
+			return this.useMethod(x);
+		});
+		info.string = function stringInfo(x) {
+			return "this is a string";
+		};
+		info.myClass = function myInfo(x) {
+			return this.nextMethod(x);
+		};
+
+		var myVal = s3.classOf("Suzanne", "myClass");
+
+		expect(function(){ info(myVal)}).toThrowError();
+	});
+
+	it("should be able to explicitly state the method name and call to a different method in the nextMethod call", function(){
+		var info = s3.generic(function info(x){
+			return this.useMethod(x);
+		});
+		info.string = function stringInfo(x) {
+			return this.nextMethod(x);
+		};
+		info.myClass = function myInfo(x) {
+			return this.nextMethod('greet', x);
+		};
+
+		var greet = s3.generic(function greet(x){
+			return this.useMethod(x);
+		});
+		greet.Suzanne = function hiSuzanne() {
+			return "it's Suzanne!";
+		};
+
+		var myVal = s3.classOf({}, ['string', 'myClass', 'Suzanne']);
+
+		expect(info(myVal)).toBe("it's Suzanne!");
+
+	});
+
+});
